0f35879e5aa77bbe6c5b534bca5256431d9c3efe
Ronaldo-j <128478364+Ronaldo-j@users.noreply.github.com>
2024-11-16 14:16:48 -0300

    Delete trabalho_web2.0 directory

--- trabalho_web2.0/backend/src/database/tabelas.py
+++
@@ -1,9 +0,0 @@
-def criar_tabelas(cursor):
-    cursor.execute("""
-        CREATE TABLE IF NOT EXISTS user(
-        id INT auto_increment PRIMARY KEY,
-        nomeUsuario VARCHAR(100) NOT NULL,
-        email VARCHAR(200) NOT NULL,
-        password VARCHAR(50) NOT NULL
-        )
-    """)
